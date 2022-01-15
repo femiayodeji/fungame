0bcf16c5235df6eb54e7acf231ecffb1a785dca4
Femi Ayodeji <femidotayodeji@gmail.com>
2022-01-15 03:16:18 +0100

    Add URI Conversion

--- contracts/Fungame.sol
+++ contracts/Fungame.sol
@@ -2,6 +2,8 @@
 
 pragma solidity ^0.8.4;
 
+import "./libraries/Base64.sol";
+
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Strings.sol";
@@ -52,24 +54,50 @@ contract FunGame is ERC721 {
         _tokenIds.increment();
     }
 
-function mintCharacterNFT(uint _characterIndex) external {
-    uint256 newItemId = _tokenIds.current();
+    function mintCharacterNFT(uint _characterIndex) external {
+        uint256 newItemId = _tokenIds.current();
 
-    _safeMint(msg.sender, newItemId);
+        _safeMint(msg.sender, newItemId);
 
-    nftHolderAttributes[newItemId] = CharacterAttributes({
-      characterIndex: _characterIndex,
-      name: defaultCharacters[_characterIndex].name,
-      imageURI: defaultCharacters[_characterIndex].imageURI,
-      hp: defaultCharacters[_characterIndex].hp,
-      maxHp: defaultCharacters[_characterIndex].maxHp,
-      attackDamage: defaultCharacters[_characterIndex].attackDamage
-    });
+        nftHolderAttributes[newItemId] = CharacterAttributes({
+        characterIndex: _characterIndex,
+        name: defaultCharacters[_characterIndex].name,
+        imageURI: defaultCharacters[_characterIndex].imageURI,
+        hp: defaultCharacters[_characterIndex].hp,
+        maxHp: defaultCharacters[_characterIndex].maxHp,
+        attackDamage: defaultCharacters[_characterIndex].attackDamage
+        });
 
-    console.log("Minted NFT w/ tokenId %s and characterIndex %s", newItemId, _characterIndex);
-    
-    nftHolders[msg.sender] = newItemId;
+        console.log("Minted NFT w/ tokenId %s and characterIndex %s", newItemId, _characterIndex);
+        
+        nftHolders[msg.sender] = newItemId;
 
-    _tokenIds.increment();
-  }    
+        _tokenIds.increment();
+    }    
+
+    function tokenURI(uint256 _tokenId) public view override returns (string memory) {
+        CharacterAttributes memory charAttributes = nftHolderAttributes[_tokenId];
+
+        string memory strHp = Strings.toString(charAttributes.hp);
+        string memory strMaxHp = Strings.toString(charAttributes.maxHp);
+        string memory strAttackDamage = Strings.toString(charAttributes.attackDamage);
+
+        string memory json = Base64.encode(
+            abi.encodePacked(
+            '{"name": "',
+            charAttributes.name,
+            ' -- NFT #: ',
+            Strings.toString(_tokenId),
+            '", "description": "This is an NFT that lets people play in the game Metaverse Slayer!", "image": "',
+            charAttributes.imageURI,
+            '", "attributes": [ { "trait_type": "Health Points", "value": ',strHp,', "max_value":',strMaxHp,'}, { "trait_type": "Attack Damage", "value": ',
+            strAttackDamage,'} ]}'
+            )
+        );
+
+        string memory output = string(
+            abi.encodePacked("data:application/json;base64,", json)
+        );
+        return output;
+    }
 }

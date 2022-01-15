ddff3fb498082c925fee258a0b5b22f14a78cddf
Femi Ayodeji <femidotayodeji@gmail.com>
2022-01-15 02:36:00 +0100

    Modify Run Script for Test

--- scripts/run.js
+++ scripts/run.js
@@ -1,6 +1,17 @@
 const main = async () => {
     const gameContractFactory = await hre.ethers.getContractFactory('FunGame');
-    const gameContract = await gameContractFactory.deploy();
+    const gameContract = await gameContractFactory.deploy(
+        [
+            "Skully", "Subzero", "Scorpion"
+        ],
+        [
+            "https://dlpng.com/png/1670173", 
+            "https://dlpng.com/png/6893905",
+            "https://www.pngall.com/?p=77369"
+        ],
+        [100, 200, 300],
+        [100, 50, 25]
+    );
     await gameContract.deployed();
     console.log("Contract deployed to:", gameContract.address);
 };
